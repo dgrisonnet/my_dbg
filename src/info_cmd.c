9078513e41a5c754aaf12ded2bc555e07d7bf7c8
Damien Grisonnet <grison_d@epita.fr>
2018-01-06 19:08:39 +0100

    [CMD] update info_regs command
    info_cmd.c : use ptrace_getregs instead of libunwind to get all the registers

--- src/info_cmd.c
+++ src/info_cmd.c
@@ -1,12 +1,12 @@
-#define UNW_LOCAL_ONLY
-
-#include <libunwind.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/user.h>
+#include <sys/ptrace.h>
 
 #include "cmd.h"
+#include "dbg.h"
 
 int do_info_memory(void *arg)
 {
@@ -30,36 +30,37 @@ int do_info_memory(void *arg)
     return 1;
 }
 
-static void info_reg(unw_cursor_t *cursor, unw_regnum_t regnum, char *reg)
-{
-    unw_word_t data;
-    unw_get_reg(cursor, regnum, &data);
-    printf("%s: 0x%lx\n", reg, (long)data);
-}
-
 int do_info_regs(void *arg)
 {
     (void)arg;
-    unw_cursor_t cursor;
-    unw_context_t uc;
-    unw_getcontext(&uc);
-    unw_init_local(&cursor, &uc);
-    info_reg(&cursor, UNW_X86_64_RIP, "rip");
-    info_reg(&cursor, UNW_X86_64_RSP, "rsp");
-    info_reg(&cursor, UNW_X86_64_RBP, "rbp");
-    info_reg(&cursor, UNW_X86_64_RAX, "rax");
-    info_reg(&cursor, UNW_X86_64_RBX, "rbx");
-    info_reg(&cursor, UNW_X86_64_RCX, "rcx");
-    info_reg(&cursor, UNW_X86_64_RDX, "rdx");
-    info_reg(&cursor, UNW_X86_64_RDI, "rdi");
-    info_reg(&cursor, UNW_X86_64_RSI, "rsi");
-    info_reg(&cursor, UNW_X86_64_R8, "r8");
-    info_reg(&cursor, UNW_X86_64_R9, "r9");
-    info_reg(&cursor, UNW_X86_64_R10, "r10");
-    info_reg(&cursor, UNW_X86_64_R11, "r11");
-    info_reg(&cursor, UNW_X86_64_R12, "r12");
-    info_reg(&cursor, UNW_X86_64_R13, "r13");
-    info_reg(&cursor, UNW_X86_64_R14, "r14");
-    info_reg(&cursor, UNW_X86_64_R15, "r15");
+    struct user_regs_struct regs;
+    ptrace(PTRACE_GETREGS, g_ctx.child_pid, NULL, &regs);
+    printf("rip: 0x%llx\n", regs.rip);
+    printf("rsp: 0x%llx\n", regs.rsp);
+    printf("rbp: 0x%llx\n", regs.rbp);
+    printf("eflags: 0x%llx\n", regs.eflags);
+    printf("orig_rax: 0x%llx\n", regs.orig_rax);
+    printf("rax: 0x%llx\n", regs.rax);
+    printf("rbx: 0x%llx\n", regs.rbx);
+    printf("rcx: 0x%llx\n", regs.rcx);
+    printf("rdx: 0x%llx\n", regs.rdx);
+    printf("rdi: 0x%llx\n", regs.rdi);
+    printf("rsi: 0x%llx\n", regs.rsi);
+    printf("r8: 0x%llx\n", regs.r8);
+    printf("r9: 0x%llx\n", regs.r9);
+    printf("r10: 0x%llx\n", regs.r10);
+    printf("r11: 0x%llx\n", regs.r11);
+    printf("r12: 0x%llx\n", regs.r12);
+    printf("r13: 0x%llx\n", regs.r13);
+    printf("r14: 0x%llx\n", regs.r14);
+    printf("r15: 0x%llx\n", regs.r15);
+    printf("cs: 0x%llx\n", regs.cs);
+    printf("ds: 0x%llx\n", regs.ds);
+    printf("es: 0x%llx\n", regs.es);
+    printf("fs: 0x%llx\n", regs.fs);
+    printf("gs: 0x%llx\n", regs.gs);
+    printf("ss: 0x%llx\n", regs.ss);
+    printf("fs_base: 0x%llx\n", regs.fs_base);
+    printf("gs_base: 0x%llx\n", regs.gs_base);
     return 1;
 }
