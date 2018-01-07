2aab3d3c5801de45e9df2242a18c97ffac1c5d01
Damien Grisonnet <grison_d@epita.fr>
2018-01-07 19:02:10 +0100

    [BREAKPOINTS] fix breakpoints reset
    bp_list.c : fix breakpoint list access
    breakpoint.c : fix ptrace calls order and add a call to ptrace single step

--- src/breakpoint.c
+++ src/breakpoint.c
@@ -1,23 +1,34 @@
 #include <sys/ptrace.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "breakpoint.h"
 #include "dbg.h"
 
 int add_breakpoint(long addr, enum bp_type type)
 {
-    long data = ptrace(PTRACE_PEEKTEXT, g_ctx.child_pid, (void *)addr, 0);
-    if (data == -1)
+    addr = 0x4000c6;
+    long data = ptrace(PTRACE_PEEKDATA, g_ctx.child_pid,
+                       (void *)addr, NULL);
+    if (data == -1) {
+        printf("Cannot access data at address 0x%lx\n", addr);
         return 0;
-    long trap = (data & 0xFFFFFF00) | 0xCC;
+    }
+    long trap = (data & ~(0xFF)) | 0xCC;
     if (ptrace(PTRACE_POKETEXT, g_ctx.child_pid, (void *)addr,
-        (void *)trap) == -1)
+               (void *)trap) == -1) {
+        printf("Cannot modify data at address 0x%lx\n", addr);
         return 0;
-    struct breakpoint bp;
+    }
+    static struct breakpoint bp;
+    memset(&bp, 0, sizeof(struct breakpoint));
     bp.addr = addr;
     bp.content = data;
     bp.type = type;
     bp.id = g_ctx.bp_list->id;
+    printf("Breakpoint %ld added at address 0x%lx\n", bp.id, addr); 
     ++g_ctx.bp_list->id;
     bplist_add(&bp.node);
     return 1;
